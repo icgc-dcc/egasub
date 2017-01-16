910c0b4a480558fc4bb29020f361ba7cec4ec5e2
Junjun Zhang <junjun.ca@gmail.com>
2017-01-16 00:02:09 -0500

    add egasub.submission module

---
+++ egasub/ega/entities/experiment.py
@@ -0,0 +1,2 @@
+clase Experiment(object):
+    pass
---
+++ egasub/submission/submit.py
@@ -0,0 +1,8 @@
+from ..ega.entities.sample import Sample
+from ..ega.entities.analysis import Analysis
+from ..ega.entities.run import Run
+from ..ega.entities.experiment import Experiment
+
+def metadata_parser(ctx, metadata_file):
+    pass
+
