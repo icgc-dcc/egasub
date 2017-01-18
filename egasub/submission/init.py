b95ea49b8b86373de64573d7874fd0fbf63983af
Brice Aminou <baminou@olm-baminou.oicr.on.ca>
2017-01-18 15:55:24 -0500

    Init function implemented

--- egasub/submission/init.py
+++ egasub/submission/init.py
@@ -5,19 +5,27 @@ import yaml
 import shutil
 
 def init_workspace(ctx):
-    username = prompt("Enter your EGA username")
-    password = prompt("Enter your EGA password",hide_input=True)
+    echo('Initalizing EGA submission workspace...')
+    #Ask user input for config.yaml
+    yaml_info = {
+        'ega_submitter_account':prompt("Enter your EGA submitter account"),
+        'ega_submitter_password':prompt("Enter your EGA submitter password",hide_input=True)
+        }
+    
     current_dir = ctx.obj['CURRENT_DIR']
-    yaml_info = {'username':username,'password':password}
+    egasub_dir = os.path.join(current_dir,'.egasub')
+    script_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     
     if os.access(current_dir, os.W_OK):
-        with open(os.path.join(current_dir,'config.yaml'),'w') as outfile:
+        os.mkdir(egasub_dir)
+        
+        policy_dir = os.path.join(current_dir,'policy')
+        
+        with open(os.path.join(egasub_dir,'config.yaml'),'w') as outfile:
             yaml.safe_dump(yaml_info,outfile,default_flow_style=False)
             
-        DATA_PATH = os.path.join(current_dir, "icgc", "services")
-        print DATA_PATH
-        print current_dir
-        shutil.copy(DATA_PATH,current_dir)
-    
-    exit()
-    echo('Initalizing EGA submission workspace...')
+        shutil.copytree(os.path.join(script_dir,"ega","data","policy"),policy_dir)
+        echo('EGA submission workspace initialized')
+        
+    else:
+        echo('Permission denied on directory '+current_dir)
