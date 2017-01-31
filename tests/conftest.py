e982ffdbd71cda4b9887a6e652c507f89487f4bc
Brice Aminou <brice.aminou@gmail.com>
2017-01-31 15:13:11 -0500

    Tests for login, logout, prepare, and submit_obj functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,45 @@
 import pytest
+import httpretty
 
+class test_ctx(object):
+    def __init__(self):
+        self.obj = {}
+        self.obj['SETTINGS'] = {}
+        self.obj['SETTINGS']['apiUrl'] = 'http://example.com/'
+        self.obj['SUBMISSION'] = {}
+        
+test_ctx = test_ctx()
+
+
+
+@pytest.fixture(scope="session")
+def ctx():
+    return test_ctx
+
+@pytest.fixture
+def pretty(ctx):
+    httpretty.enable()
+    
+    httpretty.register_uri(httpretty.POST,"%slogin" % (ctx.obj['SETTINGS']['apiUrl']),
+                       body='{"header":{"code" : "200"},"response" : {"result" : [ { "session" : { "sessionToken":"abcdefg" }}]}}',
+                       content_type="application/json")
+    
+    httpretty.register_uri(httpretty.POST,"%ssubmissions" % (ctx.obj['SETTINGS']['apiUrl']),
+                       body='{"header" : {"code" : "200"}, "response" : {"result" : [{ "id":"12345" }]}}',
+                       content_type="application/json")
+    
+    httpretty.register_uri(httpretty.DELETE, "%slogout" % (ctx.obj['SETTINGS']['apiUrl']),
+                       content_type="application/json")
+    
+    httpretty.register_uri(httpretty.POST, "%ssubmissions/12345/studies" % (ctx.obj['SETTINGS']['apiUrl']),
+                           body='{"header" : {"code" : "200"}, "response" : {"result" : [{ "id":"6789" }]}}',
+                   content_type="application/json")
+    
+    httpretty.register_uri(httpretty.PUT, "%sstudies/6789?action=VALIDATE" % (ctx.obj['SETTINGS']['apiUrl']),
+                           body='{"header" : {"code" : "200"}, "response" : {"result" : [{ "id":"12345" }]}}',
+                   content_type="application/json")
+    
+    httpretty.register_uri(httpretty.GET, "%sstudies/test_alias?idType=ALIAS" % (ctx.obj['SETTINGS']['apiUrl']),
+                           body='{"header" : {"code" : "200"}, "response" : {"result" : [{ "id":"12345" }]}}',
+                   content_type="application/json")
+    
