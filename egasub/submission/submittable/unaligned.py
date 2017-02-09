25ab93e1773597dc25e65f0c20f4119c5f680ec3
Brice Aminou <brice.aminou@gmail.com>
2017-02-09 14:32:08 -0500

    Two tests added to the submittable

--- egasub/submission/submittable/unaligned.py
+++ egasub/submission/submittable/unaligned.py
@@ -26,6 +26,7 @@ class Unaligned(Experiment):
 
             self._run.files = map(lambda file_: EFile.from_dict(file_), self.metadata.get('files'))
         except Exception, err:
+            raise Exception(err)
             raise Exception("Can not create 'unaligned' submission from this directory: %s. Please verify it's content. Error: %s" % (self._path, err))
 
     @property
