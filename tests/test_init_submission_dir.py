f4d0b80320b9b5dedafe7a1bd43eb418a7923594
Brice Aminou <brice.aminou@gmail.com>
2017-02-15 11:17:03 -0500

    Testing implementation of submission_dir

---
+++ tests/test_init_submission_dir.py
@@ -0,0 +1,36 @@
+
+from egasub.submission.init_submission_dir import init_submission_dir
+import pytest
+
+def test_alignment(ctx):
+    submission_dirs = ['test_1','test_2']
+    ctx.obj['CURRENT_DIR_TYPE'] = "alignme"
+    
+    ctx.obj['CURRENT_DIR_TYPE'] = "alignment"
+    with pytest.raises(IOError):
+        init_submission_dir(ctx,submission_dirs)
+        
+    submission_dirs = [""]
+    init_submission_dir(ctx,submission_dirs)
+
+def test_unaligned(ctx):
+    submission_dirs = ['test_1','test_2']
+    ctx.obj['CURRENT_DIR_TYPE'] = "unalign"
+    
+    ctx.obj['CURRENT_DIR_TYPE'] = "unaligned"
+    with pytest.raises(IOError):
+        init_submission_dir(ctx,submission_dirs)
+        
+    submission_dirs = [""]
+    init_submission_dir(ctx,submission_dirs)
+    
+def test_variation(ctx):
+    submission_dirs = ['test_1','test_2']
+    ctx.obj['CURRENT_DIR_TYPE'] = "variati"
+    
+    ctx.obj['CURRENT_DIR_TYPE'] = "variation"
+    with pytest.raises(IOError):
+        init_submission_dir(ctx,submission_dirs)
+        
+    submission_dirs = [""]
+    init_submission_dir(ctx,submission_dirs)
