0d9b635a0550e423bdd2fee5c41aad91c5ac98ea
Chen Chen <cchen@olm-cchen.ad.oicr.on.ca>
2017-05-05 09:49:10 -0400

    update to submittable_status

--- tests/test_ega_enums.py
+++ tests/test_ega_enums.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pytest
 import os
 from egasub.ega.entities.ega_enums import EgaEnums
@@ -6,12 +5,3 @@ from egasub.ega.entities.ega_enums import EgaEnums
 def test_load_enums():
     e = EgaEnums()
     assert len(e.lookup("analysis_types")) == 3
-=======
-import pytest
-import os
-from egasub.ega.entities.ega_enums import EgaEnums
-
-def test_load_enums():
-    e = EgaEnums()
-    assert len(e.lookup("analysis_types")) == 3
->>>>>>> e06fb7c7150bb2f460241499edefe41bd286b60d
