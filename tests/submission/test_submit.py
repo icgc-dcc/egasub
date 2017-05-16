048c582b73cc350890bcd8e84957bbea01227bac
Chen2x <chenchen199727@gmail.com>
2017-05-16 13:24:39 -0400

    test coverage

--- tests/submission/test_submit.py
+++ tests/submission/test_submit.py
@@ -1,5 +1,4 @@
 from egasub.submission.submit import submittable_status, submit_dataset, perform_submission
-from egasub.ega.services import logout
 from egasub.ega.entities.ega_enums import EgaEnums
 import pytest
 import os
@@ -10,17 +9,17 @@ def test_submittable_status():
     assert submittable_status("fail") == None
     assert submittable_status("tests/submission/test_submit.py") == ['']
 
-def test_perform_submission(ctx, mock_server):
+def test_submit(ctx, mock_server):
 
     ctx.obj['SETTINGS']['ega_submitter_account'] = 'test_account'
     ctx.obj['SETTINGS']['ega_submitter_password'] = 'test_password'
     ctx.obj['SETTINGS']['ega_policy_id'] = 'test_id'
-    ctx.obj['CURRENT_DIR'] = os.path.join('tests/data/workspace/Unaligned.20170110/ssample_y')
+    ctx.obj['CURRENT_DIR'] = os.path.join('ssample_y')
     ctx.obj['CURRENT_DIR_TYPE'] = "unaligned"
     ctx.obj['EGA_ENUMS'] = EgaEnums()
     ctx.obj['log_file'] = 'tests/data/workspace/unaligned.20170110/ssample_y/.status'
     
-    #perform_submission(ctx, '///')
+    perform_submission(ctx, '///')
 
     initial_directory = os.getcwd()
     os.chdir('tests/data/workspace/unaligned.20170110/')
@@ -32,13 +31,9 @@ def test_perform_submission(ctx, mock_server):
     with pytest.raises(Exception):
         perform_submission(ctx, ['ssample_y'])
 
-    shutil.rmtree(os.path.join(os.getcwd(), 'ssample_y/.status'))
-    os.chdir(initial_directory)
 
-
-def test_submit_dataset(ctx, mock_server):
-    #with pytest.raises(AttributeError):
-        #submit_dataset(ctx)
+    with pytest.raises(AttributeError):
+        submit_dataset(ctx)
 
     #with pytest.raises(AttributeError):
     #submit_dataset(ctx)
@@ -48,6 +43,9 @@ def test_submit_dataset(ctx, mock_server):
     ctx.obj['SETTINGS']['ega_policy_id'] = None
     ctx.obj['CURRENT_DIR'] = None
     ctx.obj['EGA_ENUMS'] = None
+
+    shutil.rmtree(os.path.join(os.getcwd(), 'ssample_y/.status'))
+    os.chdir(initial_directory)
 
 
 
